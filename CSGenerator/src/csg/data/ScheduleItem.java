6a00faf54463d55ba8bde1e89f91cee5b0b99f08
Jackie <Jackie@172.24.136.130>
2017-04-24 20:16:30 -0400

    Added additional fields to ScheduleItem

--- CSGenerator/src/csg/data/ScheduleItem.java
+++ CSGenerator/src/csg/data/ScheduleItem.java
@@ -15,14 +15,24 @@ import javafx.beans.property.StringProperty;
 public class ScheduleItem {
     private final StringProperty type;
     private final StringProperty date;
+    private final StringProperty time;
     private final StringProperty title;
     private final StringProperty topic;
+    private final StringProperty link;
+    private final StringProperty criteria;
     
     public ScheduleItem(String initType, String initDate, String initTitle, String initTopic) {
+        this(initType,initDate,"",initTitle,initTopic,"","");
+    }
+    
+    public ScheduleItem(String initType, String initDate, String initTime, String initTitle, String initTopic, String initLink, String initCriteria) {
         type = new SimpleStringProperty(initType);
         date = new SimpleStringProperty(initDate);
+        time = new SimpleStringProperty(initTime);
         title = new SimpleStringProperty(initTitle);
         topic = new SimpleStringProperty(initTopic);
+        link = new SimpleStringProperty(initLink);
+        criteria = new SimpleStringProperty(initCriteria);
     }
     
     public String getType() {
@@ -33,11 +43,23 @@ public class ScheduleItem {
         return date.get();
     }
     
+    public String getTime() {
+        return time.get();
+    }
+    
     public String getTitle() {
         return title.get();
     }
     
     public String getTopic() {
         return topic.get();
+    }
+    
+    public String getLink() {
+        return link.get();
+    }
+    
+    public String getCriteria() {
+        return criteria.get();
     }
 }
