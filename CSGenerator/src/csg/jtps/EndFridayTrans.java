6958129b03508a14e7eeab4bf262013683c1358c
Jackie <Jackie@172.24.136.130>
2017-05-06 16:01:36 -0400

    Created start Monday and end Friday transactions

---
+++ CSGenerator/src/csg/jtps/EndFridayTrans.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csg.jtps;
+
+import csg.CSGeneratorApp;
+import csg.data.CSGData;
+import java.time.LocalDate;
+import jtps.jTPS_Transaction;
+
+/**
+ *
+ * @author Jackie Quach
+ */
+public class EndFridayTrans implements jTPS_Transaction {
+
+    CSGeneratorApp app;
+    LocalDate chosenDate, startMonday, endFriday;
+    
+    public EndFridayTrans(CSGeneratorApp app, LocalDate chosenDate, LocalDate startMonday, LocalDate endFriday) {
+        this.app = app;
+        this.chosenDate = chosenDate;
+        this.startMonday = startMonday;
+        this.endFriday = endFriday;
+    }
+    
+    @Override
+    public void doTransaction() {
+        CSGData data = (CSGData)app.getDataComponent();
+        data.changeEndFriday(chosenDate,startMonday,endFriday);
+    }
+
+    @Override
+    public void undoTransaction() {
+        CSGData data = (CSGData)app.getDataComponent();
+        data.setEndFriday(endFriday);
+    }
+    
+}
